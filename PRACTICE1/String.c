73384e0739f6d19407caa9060085df2aa1263a6d
PrathamPandit1 <prathameshpandit780@gmail.com>
2026-08-11 09:37:44 +0530

    Add C practice programs

---
+++ PRACTICE1/String.c
@@ -0,0 +1,72 @@
+//ALL PREDEFINNE FUNNCTIONS IN STRING
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+//	char str1[20] = "prathamesh";
+//	char str2[20] = "pratham";
+//	printf("%lu\n",strlen(str1));//use strlen function
+//	
+//	strcpy(str1,str2);
+//	printf("%s\n",str1);//strcpy function
+//	
+//	strcat(str1," hello!!");//strcat function
+//	printf("%s",str1);
+
+
+
+//reverse function
+
+//char str1[20];
+//printf("Enter a character :");
+//scanf("%s",str1);
+//
+//strrev(str1);     //strrev function use for reverse the string
+//printf("%s",str1);
+
+
+
+
+// convert into the lowercase
+
+//char str1[20];
+//printf("Enter a name :");
+//scanf("%s",str1);
+//
+//strlwr(str1);     //	strlwr function use to convert into the lower case
+//printf("%s",str1);
+
+
+
+//convert into the uppercase
+
+//char name[60];
+//printf("Enter a name :");
+//scanf("%s",name);
+//
+//
+//strupr(name);
+//printf("%s",name);
+
+
+
+//char name[30]="Brogramming";    //mutable we can chaane any indecing character using the 
+//                                 //puts function
+//name[0] = 'P';
+//puts(name);
+
+
+                  //gets he input ghenyasathi use krta and puts he print karnyasathi.
+char name[20];
+printf("Enter a character :");
+gets(name);// gets only for string,(%d,%s,%c,%f not supported)
+puts(name);// puts only for string,(%d,%s,%c,%f not supported)
+
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ PRACTICE1/practice.c
@@ -0,0 +1,51 @@
+//print sum of in natural nimber 1 to 100 between even number sum and odd number multi
+#include<stdio.h>
+/*int main()
+{
+	int i,n,sum = 0;
+    int mul = 1;
+	printf("Enter a number :");
+	scanf("%d",&n);
+	
+	for(i = 1;i<=n;i++)
+	{
+		if(i%2==0)
+		{
+			sum = sum+i;
+		}
+		else{
+			mul = mul*i;
+		}
+	}
+	printf(" sum of natural number is %d\n",sum);
+	printf("mul of natural number is %d",mul);
+	
+return 0;
+}*/
+
+
+
+int main ()
+{
+//	int i,mul = 1;
+//	int sum =0;
+//	int name[10] = {10,2,7,6,5,6,7,8,9,5};
+////	printf("Enter a number :");
+////	scanf("%d",&name);
+//	for(i=0;i<10;i++)
+//	{
+//		if(i%2==0){
+//		
+//		sum = sum + name[i];
+//	}
+//	
+//	else{
+//		mul = mul*name[i];
+//	}
+//}
+//printf("sum %d\n",sum);
+//printf("mul %d",mul);
+	
+	
+	return 0;
+}
